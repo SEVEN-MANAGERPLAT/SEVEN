0696647c627e421cedae865b8ff2d55ee364f749
muwenliang <a56299186@163.com>
2020-06-13 08:34:48 +0800

    修改点击量

--- src/main/java/com/seven/aemp/service/IdeaclickService.java
+++ src/main/java/com/seven/aemp/service/IdeaclickService.java
@@ -3,6 +3,8 @@ package com.seven.aemp.service;
 import com.seven.aemp.bean.IdeaclickBean;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -13,4 +15,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IdeaclickService extends IService<IdeaclickBean> {
 
+    //查询点击数
+    public List<IdeaclickBean> queryIdeaclick(IdeaclickBean ideaclickBean)throws Exception;
+
+    //添加点击数
+    public void addIdeaclick(IdeaclickBean ideaclickBean)throws Exception;
+
+    //修改点击数
+    public void updateIdeaclick(IdeaclickBean ideaclickBean)throws Exception;
 }
