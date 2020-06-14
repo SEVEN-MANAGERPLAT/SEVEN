0ec10230b1c9b8d13d128962117234fefcb86cac
muwenliang <a56299186@163.com>
2020-06-14 10:22:03 +0800

    修改点击数

--- src/main/java/com/seven/aemp/dao/IdeaclickDao.java
+++ src/main/java/com/seven/aemp/dao/IdeaclickDao.java
@@ -4,6 +4,8 @@ import com.seven.aemp.bean.IdeaclickBean;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -15,4 +17,12 @@ import org.mapstruct.Mapper;
 @Mapper
 public interface IdeaclickDao extends BaseMapper<IdeaclickBean> {
 
+    //查询点击数
+    List<IdeaclickBean> queryIdeaclick(IdeaclickBean ideaclickBean);
+
+    //添加点击数
+    int addIdeaclick(IdeaclickBean ideaclickBean);
+
+    //更新点击数
+    int updateIdeaclick(IdeaclickBean ideaclickBean);
 }
