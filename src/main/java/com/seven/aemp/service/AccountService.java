23c01fb413185717e35f47aaa95ef1387a9ada21
1332165192 <1332165192@qq.com>
2020-08-29 18:29:56 +0800

    文件错误修改

--- src/main/java/com/seven/aemp/service/AccountService.java
+++ src/main/java/com/seven/aemp/service/AccountService.java
@@ -1,63 +1,52 @@
-package com.seven.aemp.service;
-
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.seven.aemp.bean.AccountBean;
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.seven.aemp.bean.GroupBean;
+import com.seven.aemp.bean.AccountBean;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 import java.util.Map;
 
-/**
- * <p>
- *  服务类
- * </p>
- *
- * @author mwl
- * @since 2019-11-19
- */
 public interface AccountService extends IService<AccountBean> {
-    //查询用户
-    public List<AccountBean> queryAccount(AccountBean accountBean);
+  //查询用户
+  public List<AccountBean> queryAccount(AccountBean accountBean);
 
-    public Page<AccountBean> queryAccount(String page, String pageSize, AccountBean accountBean) throws Exception;
+  public Page<AccountBean> queryAccount(String page, String pageSize, AccountBean accountBean) throws Exception;
 
-    //用户登录
-    public JSONObject login(AccountBean accountBean) throws Exception;
+  //用户登录
+  public JSONObject login(AccountBean accountBean) throws Exception;
 
-    //修改用户信息
-    public void updateAccount(AccountBean accountBean)throws Exception;
+  //修改用户信息
+  public void updateAccount(AccountBean accountBean)throws Exception;
 
-    //添加账户
-    public AccountBean addAccount(AccountBean accountBean) throws Exception;
+  //添加账户
+  public AccountBean addAccount(AccountBean accountBean) throws Exception;
 
-    UserDetails loadUserByUsername(String userName);
+  UserDetails loadUserByUsername(String userName);
 
-    public JSONObject loginTwo(AccountBean accountBean)throws Exception;
+  public JSONObject loginTwo(AccountBean accountBean)throws Exception;
 
-    //获取当前登录的用户信息
-    public AccountBean getCurrentAccount();
+  //获取当前登录的用户信息
+  public AccountBean getCurrentAccount();
 
-    //查询充值信息
-    public Map<String,String> queryConsumAndArrease() throws Exception;
+  //查询充值信息
+  public Map<String,String> queryConsumAndArrease() throws Exception;
 
-    //查询不同状态的客户数
-    public Map<String,String> queryAccountState() throws Exception;
+  //查询不同状态的客户数
+  public Map<String,String> queryAccountState() throws Exception;
 
-    //查询每日的点击量，消费情况
-    public List<Map<String,String>> queryConsum(AccountBean accountBean) throws Exception;
+  //查询每日的点击量，消费情况
+  public List<Map<String,String>> queryConsum(AccountBean accountBean) throws Exception;
 
-    //查询推广账户统计
-    public List<AccountBean> queryFirmSummary(AccountBean accountBean) throws Exception;
+  //查询推广账户统计
+  public List<AccountBean> queryFirmSummary(AccountBean accountBean) throws Exception;
 
 
 
-    //前台接口
-    //公共数据查询
-    public JSONObject queryTotleData() throws Exception;
+  //前台接口
+  //公共数据查询
+  public JSONObject queryTotleData() throws Exception;
 
-    //前台首页查询
-    public Page<AccountBean> queryAccoutClickNum(String page, String pageSize, AccountBean accountBean) throws Exception;
+  //前台首页查询
+  public Page<AccountBean> queryAccoutClickNum(String page, String pageSize, AccountBean accountBean) throws Exception;
 }
